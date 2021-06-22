8abd4829a053f473eb87fdaaa734a50c2f5a3c76
Ayub Idrees <ayubbscs@gmail.com>
2021-06-22 17:35:22 +0500

    Project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { LottieSplashScreen } from '@ionic-native/lottie-splash-screen/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [ LottieSplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
